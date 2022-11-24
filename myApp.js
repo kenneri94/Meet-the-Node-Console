08a0e78e25ab967d00ccce170f2dee541f871a59
Kennedy Otieno <kenneri94@gmail.com>
2022-11-24 14:54:07 -0800

    Add Hello Express to myApp

--- myApp.js
+++ myApp.js
@@ -2,8 +2,13 @@ let express = require('express');
 let app = express();
 
 
+// #1
+// console.log('Hello World')
 
-console.log('Hello World')
+// #2
+app.get("/",(req,res)=>{
+  res.send("Hello Express");
+});
 
 
 
