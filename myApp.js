e19d750825148b82e2383bb4ef2a6b737f3d5c17
Kennedy Otieno <kenneri94@gmail.com>
2022-11-25 14:59:11 -0800

    Add .env

--- myApp.js
+++ myApp.js
@@ -13,16 +13,19 @@ require('dotenv').config()
 // #7
 
 app.use((req,res,next)=>{
-  console.log(`${req.method} ${req.path}-${req.ip}`);
-  next()
-});
+  console.log(req.method + " " + req.path+ "-" + req.ip);
+  next();
+})
+
+// #4
+app.use('/public',express.static(__dirname+'/public'));
+
+
+
 // #3
 app.get("/",(req,res)=>{
   res.sendFile(__dirname+"/views/index.html");
 });
-
-// #4
-app.use('/public',express.static(__dirname+'/public'));
 
 // #5
 
