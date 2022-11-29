fee86fdb09b52fc77021fd63bc404558ae4d3663
Kennedy Otieno <kenneri94@gmail.com>
2022-11-29 07:08:43 -0800

    Add middleware and final route

--- myApp.js
+++ myApp.js
@@ -45,7 +45,14 @@ app.get('/json',(req,res)=>{
 });
 
 
+// #8 chain middleware to create a time server
 
+app.get('/now', (req,res,next)=>{
+  req.time=new Date().toString()
+  next();
+}, (req,res)=>{
+  res.json({time:req.time})
+});
 
 
 
