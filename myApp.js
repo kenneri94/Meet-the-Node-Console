0d2deb1e0a3c188dec641108b33da8bad1f6eac1
Kennedy Otieno <kenneri94@gmail.com>
2022-11-29 04:23:29 -0800

    update root level logger

--- myApp.js
+++ myApp.js
@@ -17,7 +17,7 @@ app.use('/public',express.static(__dirname+'/public'));
 // #7
 
 app.use((req,res,next)=>{
-  console.log(req.method + "  " + req.path + " - " + req.ip)
+  console.log("req.method+''+ req.path+'-'+ req.ip");
   next();
 });
 
